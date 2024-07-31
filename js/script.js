6078993e8f302f241837e635ad46fe0ac87ace07
Nataliia Balashova <88karamelka88@mail.ru>
2024-07-31 17:05:46 +0300

    added project

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const title = document.getElementsByTagName('h1')[0];
+const handlerBtn = document.getElementsByClassName('handler_btn');
+const screenBtn = document.querySelector('.screen-btn');
+const otherItemsPercent = document.querySelectorAll('.other-items.percent');
+const otherItemsNumber = document.querySelectorAll('.other-items.number');
+const input = document.querySelector('.rollback input[type=range]');
+const span = document.querySelector('.rollback span');
+const totalInputs = document.getElementsByClassName('total-input');
+let screens = document.querySelectorAll('.screen');
+
+
+for (let i = 0; i < totalInputs.length; i++) {
+    const element = totalInputs[i];
+    //console.log(element);
+}
