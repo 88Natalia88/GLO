41150c9c1feda8ece6c779138de8dbb44259428f
Nataliia Balashova <88karamelka88@mail.ru>
2024-08-01 16:54:46 +0300

    added new const

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 const title = document.getElementsByTagName('h1')[0];
 const handlerBtn = document.getElementsByClassName('handler_btn');
+const handlerBtnStart = handlerBtn[0];
+const handlerBtnReset = handlerBtn[1];
 const screenBtn = document.querySelector('.screen-btn');
 const otherItemsPercent = document.querySelectorAll('.other-items.percent');
 const otherItemsNumber = document.querySelectorAll('.other-items.number');
 const input = document.querySelector('.rollback input[type=range]');
 const span = document.querySelector('.rollback span');
 const totalInputs = document.getElementsByClassName('total-input');
+const totalInput = totalInputs[0];
+const totalInputCount = totalInputs[1];
+const totalInputExtra = totalInputs[2];
+const totalInputSum = totalInputs[3];
+const totalInputRollback = totalInputs[4];
 let screens = document.querySelectorAll('.screen');
 
-
-for (let i = 0; i < totalInputs.length; i++) {
-    const element = totalInputs[i];
-    //console.log(element);
-}
